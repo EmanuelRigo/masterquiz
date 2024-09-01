6b19f3aed1d2c5f97f6f50c40bef409da1913613
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2024-09-01 15:42:13 -0300

    commit

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -3,10 +3,7 @@ import { Link } from "react-router-dom";
 
 function CategoryCard({ category, icon, alt }) {
   return (
-    <Link
-      className="btn btn-warning p-2 p-md-4 p-lg-5"
-      to={`/category/${category}`}
-    >
+    <Link className="btn btn-warning p-2 p-md-4 " to={`/category/${category}`}>
       <div className="categorycard">{icon}</div>
     </Link>
   );
